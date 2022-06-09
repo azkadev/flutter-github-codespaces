0a83c36eaca809e5085b9c0b13926304d8b85f85
Azka Full Snack Developer:) <82513502+azkadev@users.noreply.github.com>
2022-06-09 13:49:26 +0000

    update

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 sudo apt update -y
 sudo apt-get install -y make git zlib1g-dev libssl-dev gperf cmake clang libc++-dev libc++abi-dev php-cli cmake g++
 sudo apt-get install -y clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev
+#wget https://github.com/fluttertools/fvm/releases/download/2.3.1/fvm-2.3.1-linux-x64.tar.gz
+#tar xfz *.tar.gz
+cd fvm
+chmod +x fvm
