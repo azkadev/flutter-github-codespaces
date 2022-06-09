c3c0338458281ca11d744f30c5f1b35f34b34de6
Azka Full Snack Developer:) <82513502+azkadev@users.noreply.github.com>
2022-06-09 20:29:15 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt-get install -y make git zlib1g-dev libssl-dev gperf cmake clang libc++-dev libc++abi-dev php-cli cmake g++
+sudo apt-get install -y clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev
